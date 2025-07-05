d20ac13fd66a529661cbdc64ec387283b24f824c
KALESHA VALI DOKUPARTHI <154424821+23mh1a05g0@users.noreply.github.com>
2025-07-05 12:14:11 +0530

    Time: 1 ms (99.94%), Space: 43.7 MB (23.8%) - LeetHub

--- 1510-find-lucky-integer-in-an-array/1510-find-lucky-integer-in-an-array.java
+++ 1510-find-lucky-integer-in-an-array/1510-find-lucky-integer-in-an-array.java
@@ -2,17 +2,27 @@ class Solution {
     public int findLucky(int[] arr) {
         int n = arr.length;
         int LuckyInteger = -1;
-        HashMap<Integer,Integer> map = new HashMap<>();
+        // HashMap<Integer,Integer> map = new HashMap<>();
+        // for(int num : arr){
+        //     map.put(num ,map.getOrDefault(num,0)+1);
+        // }
+        // for(Map.Entry<Integer, Integer> entry: map.entrySet()){
+        //     if(entry.getKey().equals(entry.getValue())){
+        //         LuckyInteger = entry.getKey();
+        //     }
+        // }
+        // return LuckyInteger;
+        int[] count = new int[501];
+        int luckyinteger = -1; 
         for(int num : arr){
-            map.put(num ,map.getOrDefault(num,0)+1);
+            count[num]++;
         }
-        for(Map.Entry<Integer, Integer> entry: map.entrySet()){
-            if(entry.getKey().equals(entry.getValue())){
-                LuckyInteger = entry.getKey();
+        for(int i=1;i<=500;i++){
+            if(count[i] == i){
+                luckyinteger = i;
             }
         }
-        return LuckyInteger;
-        
+        return luckyinteger;
         
         
     }
