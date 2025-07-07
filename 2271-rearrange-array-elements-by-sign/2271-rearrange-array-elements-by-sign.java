07dba69b0716a3bb96ed1800cfffac77e21d32c3
KALESHA VALI DOKUPARTHI <154424821+23mh1a05g0@users.noreply.github.com>
2025-07-07 11:03:12 +0530

    Time: 3 ms (100%), Space: 77.3 MB (41.73%) - LeetHub

--- 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.java
@@ -1,21 +1,36 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-        List<Integer> pos = new ArrayList<>();
-        List<Integer> neg = new ArrayList<>();
-        for(int num : nums){
-            if(num >= 0){
-                pos.add(num);
-            }
-            else{
-                 neg.add(num);
-            }
+    //     List<Integer> pos = new ArrayList<>();
+    //     List<Integer> neg = new ArrayList<>();
+    //     for(int num : nums){
+    //         if(num >= 0){
+    //             pos.add(num);
+    //         }
+    //         else{
+    //              neg.add(num);
+    //         }
+    //     }
+    //    int P=0,N=0,i=0;
+    //    int[] arr = new int[nums.length];
+    //     while(P < pos.size() && N < neg.size()){
+    //         arr[i++] = pos.get(P++);
+    //         arr[i++] = neg.get(N++);
+    //     }
+    //     return arr;
+    
+    int n = nums.length;
+    int[] arr = new int[n];
+    int pos = 0 , neg = 1;
+    for(int i=0;i<n;i++){
+        if(nums[i]>=0){
+            arr[pos] = nums[i];
+            pos = pos + 2;
         }
-       int P=0,N=0,i=0;
-       int[] arr = new int[nums.length];
-        while(P < pos.size() && N < neg.size()){
-            arr[i++] = pos.get(P++);
-            arr[i++] = neg.get(N++);
+        else{
+            arr[neg] = nums[i];
+            neg = neg + 2;
         }
-        return arr;
+    }
+    return arr;
     }
 }
