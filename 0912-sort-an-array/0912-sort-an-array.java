8999f814cf1cea8629628b7798a64426325cfebd
KALESHA VALI DOKUPARTHI <154424821+23mh1a05g0@users.noreply.github.com>
2026-07-22 11:36:24 +0530

    Time: 29 ms (33.64%), Space: 83.1 MB (64.36%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.java
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -1,9 +1,60 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-        //for(int i=0;i<nums.length;i++)
-        {
-            Arrays.sort(nums);
+       mergesort(nums,0,nums.length-1);
+       return nums;
+    }
+
+    public void mergesort(int[] nums, int left,int right){
+        if(left >= right){
+            return;
         }
-        return nums;
+
+        int mid = left + (right-left) / 2;
+        mergesort(nums,left,mid);
+        mergesort(nums,mid+1,right);
+        merge(nums,left,mid,right);
+    }
+
+    public void merge(int[] nums, int left,int mid,int right){
+        int n1 = mid-left+1;
+        int n2 = right-mid;
+
+        int L[] = new int[n1];
+        int R[] = new int[n2];
+
+        for(int i=0;i<n1;i++){
+            L[i] = nums[left+i];
+        }
+
+        for(int j=0;j<n2;j++){
+            R[j] = nums[mid+1+j];
+        }
+
+        int i=0;
+        int j=0;
+        int k = left;
+
+        while(i < n1 && j < n2){
+            if(L[i] <= R[j]){
+                nums[k] = L[i];
+                i++;
+            }else{
+                nums[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        while(i < n1){
+            nums[k] = L[i];
+            i++;
+            k++;
+        }
+
+        while(j < n2){
+            nums[k] = R[j];
+            j++;
+            k++;
+        }
     }
 }
