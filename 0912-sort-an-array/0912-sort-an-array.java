c045db53160655e157053b976f5c8d1d66d2db12
KALESHA VALI DOKUPARTHI <154424821+23mh1a05g0@users.noreply.github.com>
2026-07-22 10:53:27 +0530

    Time: 17 ms (97.41%), Space: 55.6 MB (100%) - LeetHub

---
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int[] sortArray(int[] nums) {
+        //for(int i=0;i<nums.length;i++)
+        {
+            Arrays.sort(nums);
+        }
+        return nums;
+    }
+}
