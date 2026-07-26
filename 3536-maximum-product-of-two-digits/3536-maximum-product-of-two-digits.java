79dd44dda6db5624e146db589e7eba74310120ae
KALESHA VALI DOKUPARTHI <154424821+23mh1a05g0@users.noreply.github.com>
2026-07-26 12:29:40 +0530

    Time: 1 ms (100%), Space: 42.9 MB (11.24%) - LeetHub

---
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxProduct(int n) {
+        ArrayList<Integer> list = new ArrayList<>();
+        while(n!=0){
+            list.add(n % 10);
+            n = n/10;
+        }
+
+        int max = 0;
+        for(int i=0;i<list.size();i++){
+            for(int j=i+1;j<list.size();j++){
+                max = Math.max(max,list.get(i)*list.get(j));
+            }
+        }
+        return max;
+    }
+}
