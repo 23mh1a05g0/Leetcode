981e451557e0108d482a41a34e0753e0d53b85b1
KALESHA VALI DOKUPARTHI <154424821+23mh1a05g0@users.noreply.github.com>
2026-07-29 22:03:23 +0530

    Time: 0 ms (100%), Space: 43.2 MB (38.67%) - LeetHub

--- 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.java
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.java
@@ -3,8 +3,8 @@ class Solution {
         char[] arr = word.toCharArray();
         int left = 0;
         int right = -1;
-        for(int i=0;i<arr.length;i++){
-              if(arr[i] == ch){
+        for(int i=0;i<word.length();i++){
+              if(word.charAt(i) == ch){
                 right = i;
                 break;
               }
