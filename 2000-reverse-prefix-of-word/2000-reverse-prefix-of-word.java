a49af3001a2959afb607471d313840c549114464
KALESHA VALI DOKUPARTHI <154424821+23mh1a05g0@users.noreply.github.com>
2026-07-29 22:02:04 +0530

    Time: 0 ms (100%), Space: 43.2 MB (23.22%) - LeetHub

---
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String reversePrefix(String word, char ch) {
+        char[] arr = word.toCharArray();
+        int left = 0;
+        int right = -1;
+        for(int i=0;i<arr.length;i++){
+              if(arr[i] == ch){
+                right = i;
+                break;
+              }
+        }
+        //right = Math.min(right,arr.length-1);
+        while(left < right){
+            char temp= arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        }
+
+        return new String(arr);
+    }
+}
