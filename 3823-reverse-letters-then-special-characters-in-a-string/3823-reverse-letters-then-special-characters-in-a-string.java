667914d1bd75d9e34fda6ae41ea446ce5c697871
KALESHA VALI DOKUPARTHI <154424821+23mh1a05g0@users.noreply.github.com>
2026-07-30 15:23:21 +0530

    Time: 2 ms (82.42%), Space: 44.4 MB (77.37%) - LeetHub

---
+++ 3823-reverse-letters-then-special-characters-in-a-string/3823-reverse-letters-then-special-characters-in-a-string.java
@@ -0,0 +1,43 @@
+class Solution {
+    public String reverseByType(String s) {
+
+        char[] arr = s.toCharArray();
+        int left = 0;
+        int right = arr.length - 1;
+        while (left < right) {
+            while (left < right && !Character.isLetter(arr[left])) {
+                left++;
+            }
+            while (left < right && !Character.isLetter(arr[right])) {
+                right--;
+            }
+            if (left < right) {
+                char temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+                left++;
+                right--;
+            }
+        }
+        left = 0;
+        right = arr.length - 1;
+
+        while (left < right) {
+
+            while (left < right && Character.isLetter(arr[left])) {
+                left++;
+            }
+            while (left < right && Character.isLetter(arr[right])) {
+                right--;
+            }
+            if (left < right) {
+                char temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+                left++;
+                right--;
+            }
+        }
+        return new String(arr);
+    }
+}
