c14090b87636dff9a8bf582bd379c24391af36c4
KALESHA VALI DOKUPARTHI <154424821+23mh1a05g0@users.noreply.github.com>
2026-08-17 22:11:11 +0530

    Time: 0 ms (100%), Space: 44 MB (8.37%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
@@ -6,7 +6,19 @@ class Solution {
         // }
         // return min;
 
-        Arrays.sort(nums);
-        return nums[0];
+        // Arrays.sort(nums);
+        // return nums[0];
+
+        int left = 0;
+        int right = nums.length - 1;
+        while(left < right){
+            int mid = left + (right - left ) / 2;
+            if(nums[mid] < nums[right]){
+                right = mid;
+            }else{
+                left = mid + 1;
+            }
+        }
+        return nums[left];
     }
 }
