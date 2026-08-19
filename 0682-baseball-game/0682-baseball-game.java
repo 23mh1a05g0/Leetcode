a20a22d18303f93c4c153e2261e145f28a5b2940
KALESHA VALI DOKUPARTHI <154424821+23mh1a05g0@users.noreply.github.com>
2026-08-19 17:57:00 +0530

    Time: 3 ms (80.77%), Space: 43.6 MB (31.11%) - LeetHub

--- 0682-baseball-game/0682-baseball-game.java
+++ 0682-baseball-game/0682-baseball-game.java
@@ -4,29 +4,27 @@ class Solution {
     public int calPoints(String[] operations) {
         Stack<Integer> stack = new Stack<>();
 
-        for (String op : operations) {
-            if (op.equals("C")) {
-                stack.pop(); 
-            } 
-            else if (op.equals("D")) {
-                stack.push(stack.peek() * 2); 
-            } 
-            else if (op.equals("+")) {
-                int last = stack.pop();
-                int secondLast = stack.peek();
-                stack.push(last);
-                stack.push(last + secondLast); 
-            } 
-            else {
-                stack.push(Integer.parseInt(op)); 
-            }
+       for(String s : operations){
+        if(s.equals("C")){
+            stack.pop();
         }
-
-        int sum = 0;
-        for (int score : stack) {
-            sum += score;
+        else if(s.equals("D")){
+            stack.push(stack.peek()*2);
         }
-
-        return sum;
+        else if(s.equals("+")){
+            int last = stack.pop();
+            int sec_last = stack.peek();
+            stack.push(last);
+            stack.push(last+sec_last);
+        }
+        else{
+            stack.push(Integer.parseInt(s));
+        }
+       }
+       int sum = 0;
+       for(int i : stack){
+        sum = sum + i;
+       }
+       return sum;
     }
 }
