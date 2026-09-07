c34ccac8ced3deca4258259b3357679a55c8e4e2
KALESHA VALI DOKUPARTHI <154424821+23mh1a05g0@users.noreply.github.com>
2026-09-07 10:16:29 +0530

    Time: 0 ms (100%), Space: 46.6 MB (72.74%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        // HashSet<ListNode> seen = new HashSet<>();
+        // ListNode curr = head;
+        // while(curr != null){
+        //     if(seen.contains(curr)){
+        //         return true;
+        //     }
+        //     seen.add(curr);
+        //     curr = curr.next;
+        // }
+        // return false;
+
+        ListNode fast = head;
+        ListNode slow = head;
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+            if(fast == slow){
+                return true;
+            }
+        }
+        return false;
+    }
+}
